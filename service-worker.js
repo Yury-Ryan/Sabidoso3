127be51382e3c756a705f3b9c8de3c715e8d3490
Yury-Ryan <yuryryanmorais@gmail.com>
2025-11-07 18:27:20 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = "sabidoso-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./CSS/style.css",
+  "./icones/icone.png",
+];
+
+// Instala o service worker e adiciona os arquivos ao cache
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+// Ativa e remove caches antigos
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((name) => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      )
+    )
+  );
+});
+
+// Responde com o cache quando offline
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
